5bf4972d0d6abaed6a522269bf64af792793d6ad
Gabriel Rojas <grojaspiano@gmail.com>
2025-11-11 19:12:38 -0600

    more SEO

--- app/page.js
+++ app/page.js
@@ -1,16 +1,64 @@
 import Image from "next/image";
+import Script from "next/script";
 import Contact from "~/components/pages/home/Contact";
 import FeaturedProducts from "~/components/pages/home/FeatruedProducts";
 import Gallery from "~/components/pages/home/Gallery";
-// import styles from "./page.module.css";
 import Hero from "~/components/pages/home/Hero";
 import MainRepairCards from "~/components/pages/home/MainRepairCards";
-import { getCollection } from "~/repositories/productRepo";
 import styles from '~/scss/pages/Home/Page.module.scss'
 
 export default async function Home() {
 
-  let collection = await getCollection();
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "RepairService",
+    name: "Rojas Repair & Refurbish",
+    url: "https://www.rojasrepair.com/",
+    image: "https://www.rojasrepair.com/logo/RRR_No_Bg.png",
+    telephone: "+1-262-909-5892",
+    priceRange: "$$",
+    areaServed: ["Racine, WI", "Kenosha, WI", "Milwaukee, WI"],
+    address: {
+      "@type": "PostalAddress",
+      streetAddress: "1552 Kearney ave",
+      addressLocality: "Racine",
+      addressRegion: "WI",
+      postalCode: "53403",
+      addressCountry: "US",
+    },
+    openingHoursSpecification: [
+      { "@type": "OpeningHoursSpecification", dayOfWeek: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"], opens: "10:00", closes: "20:00" },
+      { "@type": "OpeningHoursSpecification", dayOfWeek: "Saturday", opens: "10:00", closes: "20:00" },
+    ],
+    sameAs: [
+      "https://www.facebook.com/profile.php?id=61582216487618",
+      "https://www.instagram.com/rojasrepairrefurbish/",
+      "https://www.youtube.com/@gabrielrojas2596/shorts",
+      // "https://www.google.com/maps?cid=YOUR_GMB_CID"
+    ],
+    makesOffer: [
+      {
+        "@type": "Offer",
+        itemOffered: { "@type": "Service", name: "Smartphone Screen Replacement" },
+        availability: "https://schema.org/InStock",
+      },
+      {
+        "@type": "Offer",
+        itemOffered: { "@type": "Service", name: "Battery Replacement" },
+        availability: "https://schema.org/InStock",
+      },
+      {
+        "@type": "Offer",
+        itemOffered: { "@type": "Service", name: "Charging Port Repair" },
+        availability: "https://schema.org/InStock",
+      },
+      {
+        "@type": "Offer",
+        itemOffered: { "@type": "Service", name: "Water Damage Diagnostics" },
+        availability: "https://schema.org/InStock",
+      },
+    ],
+  };
 
   return (
     <div className={styles.page}>
@@ -29,7 +77,7 @@ export default async function Home() {
             height="500px"
             frameBorder="0"
             marginHeight="0"
-            marginWidth="0" 
+            marginWidth="0"
           >
             Loading Form...
           </iframe>
@@ -37,6 +85,11 @@ export default async function Home() {
         <br />
 
       </main>
+      <Script
+        id="rojas-localbusiness"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
 
     </div>
   );
