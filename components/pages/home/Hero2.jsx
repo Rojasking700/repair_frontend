8420ed006773d70f969e7e6fbcc3ad61e11a6cbb
Gabriel Rojas <grojaspiano@gmail.com>
2025-11-20 16:40:09 -0600

    verbaige

--- components/pages/home/Hero2.jsx
+++ components/pages/home/Hero2.jsx
@@ -6,7 +6,14 @@ export default function Hero2() {
   return (
     <div className={styles.hero2Container} >
       <div>
-
+        <h1>Give Your Tech Another Life.</h1>
+        <h3>Fast, Professional phone, tablet and computer repair -without the upsell</h3>
+        <ul>
+          <li>Honest Daignostics</li>
+          <li>Fair Pricing</li>
+          <li>Quality Service</li>
+        </ul>
+        <button>Book a repair</button>
       </div>
       <div>
 
