c622d952468d341ef717c1d2e1db09b7b40e4545
Gabriel Rojas <grojaspiano@gmail.com>
2025-11-20 19:15:58 -0600

    clean up

--- components/pages/home/MainRepairCards.jsx
+++ components/pages/home/MainRepairCards.jsx
@@ -1,15 +1,8 @@
 'use client'
 import ProductCard from "~/components/shared/products/productCard";
 import styles from '~/scss/pages/Home/Page.module.scss'
-import useEmblaCarousel from 'embla-carousel-react';
-import { usePrevNextButtons, UseDotButton } from '~/components/shared/embla/EmblaButtons';
 
 export default function MainRepairCards({ collection }) {
-
-  // let shopProducts = collection?.products?.edges?.length > 0 ? collection?.products?.edges : [];
-
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, align: 'start', container: `.${styles.cardSectionCards}`, })
-  const { PrevButton, NextButton, } = usePrevNextButtons(emblaApi);
 
   let shopProducts = [
     {
