992d5b433baf51bb19b5551a331604e1e51446ed
Gabriel Rojas <grojaspiano@gmail.com>
2025-11-25 18:47:34 -0600

    gtm pt 2

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import NavBar from "@/components/nav/NavBar";
 import Footer from "~/components/footer/footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -102,7 +102,19 @@ export default function RootLayout({ children }) {
         <Footer />
       </body>
 
-      <GoogleAnalytics gaId="G-GTT4JK6FMT" />
+        <Script
+          id="gtm-base"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','G-GTT4JK6FMT');
+            `,
+          }}
+        />
       <SpeedInsights />
       <Analytics />
     </html>
